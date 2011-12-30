80982ec5385cc8ed09b35325bd83061f64139c16
cobreti <devnull@localhost>
2011-12-30 14:39:31 -0500

    fixed linux NyxModule imp

--- NyxBase/Linux/Source/NyxModule_Impl.cpp
+++ NyxBase/Linux/Source/NyxModule_Impl.cpp
@@ -1,1 +1,83 @@
-/home/dannyt/dev/NyxLibs/NyxBase/OSX/Source/NyxModule_Impl.mm
+#include "NyxModule_Impl.hpp"
+#include "NyxTraceStream.hpp"
+
+Nyx::CModule* NyxLinux::CModule_Impl::s_pDefaultModule = NULL;
+
+
+/**
+ *
+ */
+Nyx::CModuleRef Nyx::CModule::Alloc()
+{
+	return new NyxLinux::CModule_Impl();
+}
+
+
+/**
+ *
+ */
+Nyx::CModule* Nyx::CModule::GetDefault()
+{
+	return NyxLinux::CModule_Impl::s_pDefaultModule;
+}
+
+
+/**
+ *
+ */
+NyxLinux::CModule_Impl::CModule_Impl()
+{
+	s_pDefaultModule = this;
+	Init();
+}
+
+
+/**
+ *
+ */
+NyxLinux::CModule_Impl::~CModule_Impl()
+{
+	Terminate();
+	s_pDefaultModule = NULL;
+}
+
+
+/**
+ *
+ */
+void NyxLinux::CModule_Impl::Init()
+{
+	m_ThreadModule.Init();
+	m_TraceModule.Init();
+    
+}
+
+
+/**
+ *
+ */
+void NyxLinux::CModule_Impl::Terminate()
+{
+	m_TraceModule.Terminate();
+	m_ThreadModule.Terminate();
+}
+
+
+/**
+ *
+ */
+Nyx::CTraceModule& NyxLinux::CModule_Impl::Traces()
+{
+	return m_TraceModule;
+}
+
+
+/**
+ *
+ */
+Nyx::CThreadModule& NyxLinux::CModule_Impl::Threads()
+{
+	return m_ThreadModule;
+}
+
+
