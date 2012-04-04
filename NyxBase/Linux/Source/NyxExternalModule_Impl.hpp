815abed537547567ac720d054ea6e0c13eb89e1d
dannyt <dannyt@FedoraG.DANHOME>
2012-04-04 18:39:42 -0400

    Added NyxExternalModule object

---
+++ NyxBase/Linux/Source/NyxExternalModule_Impl.hpp
@@ -0,0 +1,32 @@
+#ifndef _NYXEXTERNALMODULE_IMPL_HPP_
+#define _NYXEXTERNALMODULE_IMPL_HPP_
+
+#include "NyxExternalModule.hpp"
+#include "NyxObject_Impl.hpp"
+#include "NyxAString.hpp"
+
+namespace NyxLinux
+{
+	/**
+	 *
+	 */
+	class CExternalModule_Impl : public Nyx::CObject_Impl<Nyx::CExternalModule>
+	{
+	public:
+		CExternalModule_Impl(const char* module);
+		virtual ~CExternalModule_Impl();
+
+		virtual Nyx::NyxResult Load();
+		virtual Nyx::NyxResult Unload();
+		virtual void* GetFct(const char* name);
+
+	protected:
+
+		Nyx::CAString		m_Module;
+		void*				m_pHandle;
+	};
+}
+
+
+#endif // _NYXEXTERNALMODULE_IMPL_HPP_
+
---
+++ include/NyxExternalModule.hpp
@@ -0,0 +1,28 @@
+#ifndef _NYXEXTERNALMODULE_HPP_
+#define _NYXEXTERNALMODULE_HPP_
+
+#include "NyxObject.hpp"
+#include "NyxRef.hpp"
+
+DECLARE_NYXOBJECTREF(CExternalModule)
+
+namespace Nyx
+{
+	/**
+	 *
+	 */
+	class CExternalModule : public Nyx::CObject
+	{
+	public:
+
+		static CExternalModule* Alloc(const char* szModule);
+
+	public:
+
+		virtual NyxResult Load() = 0;
+		virtual NyxResult Unload() = 0;
+		virtual void* GetFct(const char* name) = 0;
+	};
+}
+
+#endif // _NYXEXTERNALMODULE_HPP_
