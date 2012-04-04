b81da1cf0f271997c2e6cfabdae72e0190c2d4cb
dannyt <dannyt@WinDev.DanHome>
2012-04-04 19:44:07 -0400

    added NyxExternalModule implementation for Win32

---
+++ NyxBase/Win32/Source/NyxExternalModule_Impl.cpp
@@ -0,0 +1,79 @@
+#include "NyxExternalModule_impl.hpp"
+
+
+/**
+ *
+ */
+Nyx::CExternalModule* Nyx::CExternalModule::Alloc(const char* szModule)
+{
+    return new NyxWin32::CExternalModule_Impl(szModule);
+}
+
+
+namespace NyxWin32
+{
+    /**
+     *
+     */
+    CExternalModule_Impl::CExternalModule_Impl(const char* szModuleFilename) :
+    m_ModuleFilename(szModuleFilename),
+    m_hModule(NULL)
+    {
+    }
+
+
+    /**
+     *
+     */
+    CExternalModule_Impl::~CExternalModule_Impl()
+    {
+        Unload();
+    }
+
+
+    /**
+     *
+     */
+    Nyx::NyxResult CExternalModule_Impl::Load()
+    {
+        if ( m_hModule != NULL )
+            return Nyx::kNyxRes_Failure;
+
+        Nyx::NyxResult  ret = Nyx::kNyxRes_Success;
+
+        m_hModule = ::LoadLibraryA(m_ModuleFilename.c_str());
+        if ( m_hModule == NULL )
+            ret = Nyx::kNyxRes_Failure;
+
+        return ret;
+    }
+
+
+    /**
+     *
+     */
+    Nyx::NyxResult CExternalModule_Impl::Unload()
+    {
+        Nyx::NyxResult  ret = Nyx::kNyxRes_Success;
+
+        if ( m_hModule != NULL )
+        {
+            ::FreeLibrary(m_hModule);
+            m_hModule = NULL;
+        }
+
+        return ret;
+    }
+
+
+    /**
+     *
+     */
+    void* CExternalModule_Impl::GetFct(const char* name)
+    {
+        if ( m_hModule == NULL )
+            return NULL;
+
+        return ::GetProcAddress(m_hModule, name);
+    }
+}
---
+++ NyxBase/Win32/Source/NyxExternalModule_Impl.hpp
@@ -0,0 +1,30 @@
+#ifndef _NYXEXTERNALMODULE_IMPL_HPP_
+#define _NYXEXTERNALMODULE_IMPL_HPP_
+
+#include "NyxExternalModule.hpp"
+#include "NyxObject_Impl.hpp"
+#include "NyxAString.hpp"
+
+namespace NyxWin32
+{
+    /**
+     *
+     */
+    class CExternalModule_Impl : public Nyx::CObject_Impl<Nyx::CExternalModule>
+    {
+    public:
+        CExternalModule_Impl(const char* szModuleFilename);
+        virtual ~CExternalModule_Impl();
+
+        virtual Nyx::NyxResult Load();
+        virtual Nyx::NyxResult Unload();
+		virtual void* GetFct(const char* name);
+
+    protected:
+
+        Nyx::CAString       m_ModuleFilename;
+        HMODULE             m_hModule;
+    };
+}
+
+#endif // _NYXEXTERNALMODULE_IMPL_HPP_
